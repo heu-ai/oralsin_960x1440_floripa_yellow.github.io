8452213ab7fca91fd21ecaca42d7ecbaec4993ab
hariheu <63750450+hariheu@users.noreply.github.com>
2021-06-29 18:51:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var whatsapp_links = "https://api.whatsapp.com/send?phone=554884237975&text=Ol%C
 var privacy_policy_link = "https://www.oralsin.com.br/politica_de_privacidade"
 
 // Subject and Recepients
-var subject =  "Oral Sin Creative"
+var subject =  "Oral Sin Creative - Floripa"
 var receivers = "leadnbids@gmail.com, analytics@infuseads.com"
 
 
